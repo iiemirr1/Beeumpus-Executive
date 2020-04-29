fc93363b375838ca114f981ee37a39deef74f8dc
Glitch (alpha-yeni-royal) <none>
2020-04-29 16:36:28 +0000

    🚴🍪 Checkpoint ./komutlar/discordjs.js:11195635/184

--- komutlar/discordjs.js
+++ komutlar/discordjs.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args, guild, user) => {
 
   try {
       await (sahip.addRole(jsrolu.id));
-      message.reply('Başarıyla **JavaScript** rolü verilmiştir <a:tk:660971421513809949>')
+      message.channel.sendMessage('<a:kayt:705092959351210074> | JavaScript rolü verilmiştir')
     } catch (error) { // eğer hata olursa bu hatayı öğrenmek için hatayı konsola gönderelim.
       return message.channel.send('Rolü verirken bir hata ortaya çıktı. Bu hatalar şunlar olabilir:\n\n1-) Botun rolü, komudu kullanan kişinin üstünde değil.\n2-) Bota gerekli yetkileri vermemişsin.\n3-) Bu rol zaten sende bulunmakta.\n\nEğer bunları yaptıysan ve olmadıysa, ** Baran #0972 **\'e ulaş.')
    
