ab71a61d7a865d2dac70a26cbf91737d505c7e86
Glitch (alpha-yeni-royal) <none>
2020-05-02 15:02:29 +0000

    🚘💫 Checkpoint ./komutlar/terfi-et.js:11195635/1976 ./komutlar/yardım.js:11195635/4247

--- komutlar/terfi-et.js
+++ komutlar/terfi-et.js
@@ -11,18 +11,10 @@ module.exports.run = async (bot, message, args) => {
     let aRole = message.mentions.roles.first()
     if (!aRole) return message.channel.sendEmbed(new Discord.RichEmbed().setDescription(`Bu rolü bulamıyorum.\nÖrnek: -terfi [@İsim] [@Yetki]`).setColor(10038562));
   
-    let rol = message.mentions.roles.first()  
-    let user = message.guild() 
-
-    if (!user) return message.channel.send(`:red: Kimden rolü alacağımı etiketle.`).catch(console.error);
-    if (!rol) return message.channel.send(`:red: Rolu etiketlemelisin`);
-    message.channel.send(`Başarıyla \`${user.user.tag}\` kişisinden \`${rol.name}\` rolü alındı. :tick2:`);
-    user.removeRole(rol);
-
-
     if (kullanıcı.roles.has(aRole.id)) return message.channel.sendEmbed(new Discord.RichEmbed().setDescription('Bu kullanıcı zaten bu terfiyeye ulaşmış').setColor(10038562));
     await (kullanıcı.addRole(aRole.id))
     message.channel.sendEmbed(new Discord.RichEmbed().setDescription(`**${kullanıcı}** isimli üye \`\`${role.name}\`\` tagına terfi etti.`).setColor('RANDOM'));
+  
 
 };
 
--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,30 +1,32 @@
 const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
-
-let botid = ('681231884767985694') //bu yere botun id'sini yapıştırın.
-//eğer botunuz dbl(discord bot list) de yoksa Bota Oy Ver (Vote) olmucaktır.
-
+var prefix = ayarlar.prefix;
 exports.run = (client, message, args) => {
-    const embed = new Discord.RichEmbed()
-        .setAuthor(`${client.user.username} `, client.user.avatarURL)
-        .setColor('#00ff1f')
-        .setTitle(`${client.user.username} - Komutlar`)
-        .setDescription(`**${ayarlar.prefix}yetkili** Yetkili Komutları.\n **${ayarlar.prefix}kullanıcı** Kullanıcı Komutları.`)  
-        .setImage("")  
-    return message.channel.sendEmbed(embed);
   
-  
+    const juke = new Discord.RichEmbed()
+    .setColor('Premium Yardım')
+    .setAuthor(`Botadı`, client.user.avatarURL) 
+      .setDescription('**[Website]() [destek sunucumuz]() [oyver]()**')
+.setThumbnail(client.user.avatarURL)
+      .addField('** Yetkili ()**', '`davet`, `istatistik`, `sor`, `afk`, `avatar`, `emojiler`, `roller`, `jumbo`, `kullanıcı-bilgi`, `ping`, `rol-bilgi`, `sunucu`, `sunucuresmi`')
+      .addField('** Yetkili ()**', '`küfür`, `modlog`, `otorol`, `otoselam`, `reklam`, `sayaç`, `sil-üye`, `sil`, `vkanal`, `yasakla`, `yaz`')
+      .addField('** Kullanıcı ()**', '`beyaz`, `kara`, `eval`, `reboot`')
+    .setFooter(``, client.user.avatarURL)
+    .setTimestamp()
+    message.channel.send(juke).catch()
+
 };
 
 exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: ['yar','help'],
-  permLevel: 0,
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
 };
 
+
 exports.help = {
-  name: 'yardım',
-  description: '',
-  usage: ''
+    name: 'yardım',
+      category: 'Yardım',
+      description: 'Yardım kategorilerini gösteir.',
 };
