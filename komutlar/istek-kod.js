ccb1f5b10fbfa3908aaca2c9324b614ea2a7e6ac
Glitch (alpha-yeni-royal) <none>
2020-05-03 08:58:36 +0000

    🐣🙈 Checkpoint ./komutlar/istek-kod.js:11195635/2393

--- komutlar/istek-kod.js
+++ komutlar/istek-kod.js
@@ -2,18 +2,17 @@ const Discord = require('discord.js');
 
 exports.run = async(client, message, args) => {
     let type = args.slice(0).join(' ');
-    if (type.length < 1) return message.channel.send('Doğru Kullanım: !istek-kod Bence bunu bunu eklerseniz daha iyi olur')
+    if (type.length < 1) return message.channel.send('Doğru Kullanım : -istekkod sayaç komutu')
 const embed = new Discord.RichEmbed()
-.setColor('RANDOM')
+.setColor('#ff2345')
 .setDescription('🎉 **İstek Kodunuz başarıyla bildirildi!** ')
-message.channel.send(embed)
 const embed2 = new Discord.RichEmbed()
-.setColor("RANDOM")
+.setColor("#ff2345")
 .setDescription(`**${message.author.tag}** adlı kullanıcının isteği:`)
 .addField(`Kulanıcı Bilgileri`, `Kullanıcı ID: ${message.author.id}\nKullanıcı Adı: ${message.author.username}\nKullanıcı Tagı: ${message.author.discriminator}`)
 .addField("İstek Kodu", type)
 .setThumbnail(message.author.avatarURL)
-client.channels.get('662331853943996446').send(embed2); // Kanal ID 
+client.channels.get('705111758897152010').send(embed2); // Kanal ID 
 
 };
 
@@ -25,7 +24,7 @@ exports.conf = {
 }
 
 exports.help = {
-    name: 'istek-kod',
+    name: 'istekkod',
     description: 'Sunucuya giren kullanıcıya seçtiğiniz rolü otomatik verir.',
     usage: 'istek-kod <istek>'
 }
