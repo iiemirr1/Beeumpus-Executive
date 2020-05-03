84a982543f08c8e49be02e33cba80e99ccb7b50d
Glitch (alpha-yeni-royal) <none>
2020-05-03 09:45:34 +0000

    🐆🌗 Checkpoint ./komutlar/booster-bilgi.js:11195635/559 ./komutlar/kurallar.js:11195635/6623

--- komutlar/booster-bilgi.js
+++ komutlar/booster-bilgi.js
@@ -5,28 +5,28 @@ let botid = ('')
 exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
     .setColor("RANDOM")
-    .addField(`Booster Rolü Bilgileri`,`Booster Rolü Almak İçin Sunucuya Boost Basmanız Gereklidir.`)
-    .addField(`Booster Avantajları`, `💸 | ▸Server Booster◂ Rolü Avantajları !
+    .addField(`**Booster Rolü Bilgileri**`,`Booster Rolü Almak İçin Sunucuya Boost Basmanız Gereklidir.`)
+    .addField(`**Booster Avantajları**`, `💸 | ▸Server Booster◂ Rolü Avantajları !
 
- <:ortak:705813855481233480>  | Sadece Boosterlere Özel 💸 | ▸Server Booster◂ Rolü Alırsınız. ( Süre : Boost Süresi Bitene Kadar )
+ <a:disco:706438922317856839>  | Sadece Boosterlere Özel 💸 | ▸Server Booster◂ Rolü Alırsınız. ( Süre : Boost Süresi Bitene Kadar )
 
- <:ortak:705813855481233480>  | Hesabınıza 45 Adet Davet Eklenir. ( Kalıcı )
+ <a:disco:706438922317856839>  | Hesabınıza 45 Adet Davet Eklenir. ( Kalıcı )
 
- <:ortak:705813855481233480>  | 2x Boost Basarsanız 50 Davetlik 💎 | ▸Premium◂ Rolünü Direk Alırsınız. ( Süre : Kalıcı )
+ <a:disco:706438922317856839>  | 2x Boost Basarsanız 50 Davetlik 💎 | ▸Premium◂ Rolünü Direk Alırsınız. ( Süre : Kalıcı )
 
- <:ortak:705813855481233480>  | 💸 | ▸Server Booster◂ Süreniz Bittiğinde , 🔥 | ▸Özel Üye◂ Rolünü Alırsınız. ( Süre : Kalıcı )
+ <a:disco:706438922317856839>  | 💸 | ▸Server Booster◂ Süreniz Bittiğinde , 🔥 | ▸Özel Üye◂ Rolünü Alırsınız. ( Süre : Kalıcı )
 
- <:ortak:705813855481233480>  | İstediğiniz Kadar Kendi Adınızı Değiştirebilirsiniz.
+ <a:disco:706438922317856839>  | İstediğiniz Kadar Kendi Adınızı Değiştirebilirsiniz.
 
- <:ortak:705813855481233480>  | Size Özel Sohbet Kanalları .
+ <a:disco:706438922317856839>  | Size Özel Sohbet Kanalları .
 
- <:ortak:705813855481233480>  | Size Özel İstek Log Kanalı.
+ <a:disco:706438922317856839>  | Size Özel İstek Log Kanalı.
 
- <:ortak:705813855481233480>  | Adınızın Yanına Booster Sembolü.
+ <a:disco:706438922317856839>  | Adınızın Yanına Booster Sembolü.
 
- <:ortak:705813855481233480>  | Yardımlarda Öncelik.
+ <a:disco:706438922317856839> | Yardımlarda Öncelik.
 
- <:ortak:705813855481233480>  | Son Olarak , Everyone Ve Here'siz 2 Günlük Reklam !`)
+ <a:disco:706438922317856839>  | Son Olarak , Everyone Ve Here'siz 2 Günlük Reklam !`)
     message.channel.sendEmbed(embed);
 
 };
