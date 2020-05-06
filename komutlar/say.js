634bc65ac72ec5519c814c3e3eaa1d0961a230da
Glitch (alpha-yeni-royal) <none>
2020-05-06 12:02:50 +0000

    🛥🚘 Checkpoint ./komutlar/say.js:11195635/5

--- komutlar/say.js
+++ komutlar/say.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 exports.run = async (client, message, args) => {
-    let tag = "£" // tagınız
+    let tag = "𝓒𝓢" // tagınız
     const voiceChannels = message.guild.channels.filter(c => c.type === 'voice');
     let count = 0;
     for (const [id, voiceChannel] of voiceChannels) count += voiceChannel.members.size;
