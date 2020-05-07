6b690fb144e88454fb1627dcc1abf4c8f816ba4c
Glitch (alpha-yeni-royal) <none>
2020-05-07 12:55:20 +0000

    👌🎭 Checkpoint ./komutlar/kayıt.js:11195635/33

--- komutlar/kayıt.js
+++ komutlar/kayıt.js
@@ -5,6 +5,7 @@ const db = require('quick.db');
 exports.run = async (client, message, args) => {
 
   const codeshare = await db.fetch(`kayıtk_${message.guild.id}`)
+  const tag = "𝓒𝓢";
   if(codeshare == null) return message.channel.send('Kayıt Sistemi eklemek için <`-kayıt-rol @rol`/`-kayıt-kanal #kanal`/`-kayıt-log #kanal`> şeklinde ekleyebilirsiniz. ');
   if (message.channel.id !== codeshare) return message.channel.send(`Kayıt Kanalı <#${codeshare}> Şuanda Burası !`);
   if (codeshare == true) return; 
@@ -20,7 +21,7 @@ exports.run = async (client, message, args) => {
   if (!isim) return message.channel.send(`İsmini girmelisin.`)
   if (!yas) return message.channel.send(`Yaşını girmelisin.`)
   
-  user.setNickname(`${isim} | ${yas}`)
+  user.setNickname(`${tag} ${isim} | ${yas}`)
   user.addRole(db.fetch(`kayıt_${message.guild.id}`))
   message.channel.send(`${message.author} Sunucuya Başarıyla Kayıt oldun !`)
   message.guild.channels.get(db.fetch(`kayıtlog_${message.guild.id}`)).send(`<a:kayit:707928076926976061> ${message.author} Adlı kullanıcı Başarılı Şekilde Kayıt Oldu ! <a:oldu:707928398902853713> `);
