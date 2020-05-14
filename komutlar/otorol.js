5a05bb473e512ff8f0cc7ec978dca7633e286bac
Glitch (alpha-yeni-royal) <none>
2020-05-14 11:33:29 +0000

    👟🚗 Checkpoint ./komutlar/otorol.js:11195635/4205

--- komutlar/otorol.js
+++ komutlar/otorol.js
@@ -1,37 +1,47 @@
-const discord = require('discord.js')
+const Discord = require('discord.js')
 const db = require('quick.db');
+const ayarlar = require('../ayarlar.json')
+
 exports.run = async(client, message, args) => {
-  if (!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send(':no_entry: Otorol ayarlamak için `Rolleri Yönet` yetkisine sahip olmanız gereklidir.')
- 
-if (args[0] == 'ayarla') {
-   var rol1;
-  var rol2;
-let anarol= message.mentions.roles.first() || message.guild.roles.get(args.join(' '));
- 
-  if (!anarol) return message.channel.send( ' Sunucuya katılan üyelere vereceğim rolü etiketlemelisiniz. Kullanım: `-otorol ayarla @Rol #kanal`')
-  else rol1 = message.mentions.roles.first().id
- 
-      let otorolismi = message.mentions.roles.first().name  
-  let kanal = message.mentions.channels.first();
+
+      if (!message.guild) {
+    const ozelmesajuyari = new Discord.RichEmbed()
+    .setColor(0x2488E7)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .addField('Sadece herhangi bir sunucudan mesaj gönderebilirim.:relaxed: ')
+    return message.author.sendEmbed(ozelmesajuyari); }
+
+  if (!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send(':no_entry: Otorol ayarlamak için `Rolleri Yönet` yetkisine sahip olman gerek.')
+
   
-  if (!kanal) return message.channel.send( 'Lütfen bilgilendirme mesajlarını atacağım yeri belirtiniz. Doğru kullanım: `!otorol ayarla @Rol #kanal`')
-    
-  db.set(`otorolIsmi_${message.guild.id}`, otorolismi)
-  
-  let logkanali = await  db.set(`otorolkanali_${message.guild.id}`, message.mentions.channels.first().id)
-  
-  let otorol = await db.set(`otorol_${message.guild.id}`, rol1)
-  
-  if (!message.guild.roles.get(rol1)) return message.channel.send("Etiketlediğiniz rol bulunamadı. Rolün etiketlenebilir olduğundan emin olmalısınız.")
-    message.channel.send(`Sunucuya yeni katılanlara verilecek rol \`${otorolismi}\` olarak ayarlandı.\n Bilgilendirme kanalı ise  olarak ayarlandı.`)  
+    if (args[0] == 'ayarla') {
+ let rol = message.mentions.roles.first() || message.guild.roles.get(args.join(' '))
+  let newRole;
+  let tworole;
+  if (!rol) return message.channel.send(':no_entry: Otorol ayarlamanız için bir rol etiketlemeniz gerek. `/otorol ayarla @Üye #kanal`')
+  else newRole = message.mentions.roles.first().id
+  let isim = message.mentions.roles.first().name  
+  let otorolkanal = message.mentions.channels.first();
+  if (!otorolkanal) return message.channel.send(':no_entry: Otorol ayarlamanız için bir rol etiketlemeniz gerek. `g!otorol ayarla @Üye #kanal`')
+    db.set(`otorolisim_${message.guild.id}`, isim)
+  let i = await  db.set(`otorolKanal_${message.guild.id}`, message.mentions.channels.first().id)
+  let otorol = await db.set(`autoRole_${message.guild.id}`, newRole)
+  if (!message.guild.roles.get(newRole)) return message.channel.send(":no_entry: Etiketlediğiniz rol bulunamadı, etiketlediğiniz rolün etiketlenebilirliğinin aktif olduğundan emin olunuz.")
+    message.channel.send(`Otorol, <@&${newRole}> mesaj kanalı <#${i}> olarak ayarlandı.`)  
      
   } 
+
   if (args[0] == 'kapat') {
     
-db.delete(`otorolIsmi_${message.guild.id}`)
-        db.delete(`otorolkanali_${message.guild.id}`)
-    db.delete(`otorol_${message.guild.id}`)
-    message.channel.send(`Otorol sistemi başarıyla kapatıldı.`)
+
+    
+    
+    db.delete(`otorolisim_${message.guild.id}`)
+        db.delete(`otorolKanal_${message.guild.id}`)
+    db.delete(`autoRole_${message.guild.id}`)
+
+    message.channel.send(`Otorolü başarıyla kapattım.`)
   }
 };
   
@@ -40,11 +50,12 @@ db.delete(`otorolIsmi_${message.guild.id}`)
 exports.conf = {
     enabled: true,
     guildOnly: true,
-    aliases: ["oto-rol"],
+    aliases: ['oto-rol'],
     permLevel: 0
 }
+
 exports.help = {
     name: 'otorol',
-    description: 'Sunucuya katılanlara belirtilen rolü verir.',
-    usage: 'otorol ayarla @rol #kanal'
+    description: 'Sunucuya giren kullanıcıya seçtiğiniz rolü otomatik verir.',
+    usage: 'otorol <@rol>'
 }
