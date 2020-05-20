0fe6c4f9a62eb448f99116c20a802b1f27bdaab9
Glitch (alpha-yeni-royal) <none>
2020-05-20 09:54:59 +0000

    🎱✋ Checkpoint ./komutlar/k-sistemi.js:11195635/25 ./komutlar/oylama.js:11195635/124

--- komutlar/k-sistemi.js
+++ komutlar/k-sistemi.js
@@ -6,7 +6,8 @@ exports.run = (client, message, args) => { //<3
    let yasin = new Discord.RichEmbed()
    .setAuthor( client.user.username, client.user.avatarURL )
    .setFooter( client.user.username, client.user.avatarURL )
-   .setDescription(' **Kayıt Sistemi Kullanımı** \n\n • `t+kayit-kanal-ayarla #kanal` 》 \n Üyelerin Kayıt Olacağı Kanal \n\n • `t+kanal-ayarla #kanal` 》 \n Üyeler Kayıt Olunca Bildirim Gidecek Kanal \n\n • `t+isim-sistemi -uye- -yas-` 》 \n Üye İsim Sistemi Özgürce Yerleştirilen Değişkenler -uye- -yas- Yaş Seçeneği Eklemezseniz Yaş Sormayacaktır. \n\n • `t+kayıt-verilecek-rol-ayarla @rol` 》 \n Kayıt Olan Kişiye Verilecek Rol \n\n • `t+kayıt-alınacak-rol-ayarla @rol` 》 \n Kayıt Olan Kişiden Alınacak Rol \n\n • `t+kayıt-sistemi-kapat` 》 \n Kayıt Sistemini Kapatır Ve Verileri Siler \n\n • `t+giriş-sistemi Hoşgeldin -uye- Kayıt Olabilmek İçin t+kayıt Baran 16` 》 \n Giriş Sistemini Editleyebilirsiniz Değişkenler -uye- -uyetag- ')
+   .setTimestamp()
+   .setDescription(' **Kayıt Sistemi Kullanımı** \n\n • `t+kayit-kanal-ayarla #kanal`  \n Üyelerin Kayıt Olacağı Kanal \n\n • `t+kanal-ayarla #kanal`  \n Üyeler Kayıt Olunca Bildirim Gidecek Kanal \n\n • `t+isim-sistemi -uye- -yas-`  \n Üye İsim Sistemi Özgürce Yerleştirilen Değişkenler -uye- -yas- Yaş Seçeneği Eklemezseniz Yaş Sormayacaktır. \n\n • `t+kayıt-verilecek-rol-ayarla @rol`  \n Kayıt Olan Kişiye Verilecek Rol \n\n • `t+kayıt-alınacak-rol-ayarla @rol`  \n Kayıt Olan Kişiden Alınacak Rol \n\n • `t+kayıt-sistemi-kapat`  \n Kayıt Sistemini Kapatır Ve Verileri Siler \n\n • `t+giriş-sistemi Hoşgeldin -uye- Kayıt Olabilmek İçin t+kayıt Baran 16` 》 \n Giriş Sistemini Editleyebilirsiniz Değişkenler -uye- -uyetag- ')
  message.channel.send(yasin)
 
 
