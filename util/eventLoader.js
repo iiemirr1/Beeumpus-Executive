33d6433a12273a76a704576f08480a770a3f9c4c
Glitch (titanx-bot) <none>
2020-05-21 17:45:39 +0000

    🎩🏮 Checkpoint ./events/çıkış.js:11195635/1944 ./events/giriş.js:11195635/2723 ./util/eventLoader.js:11195635/757 ./komutlar/resimli-hgbb.js:11195635/928 ./package.json:11195635/9352

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -1,5 +1,7 @@
 const reqEvent = (event) => require(`../events/${event}`);
 module.exports = client => {
+  client.on('guildMemberAdd', () => reqEvent('giriş'));
+  client.on('guildMemberRemove', reqEvent('çıkış'));
   client.on('ready', () => reqEvent('ready')(client));
   client.on('message', reqEvent('message'));
 };
