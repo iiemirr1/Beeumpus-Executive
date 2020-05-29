9011d2b6095d923ef7f9b2c14a4794622d1ab29a
Glitch (titanx-bot) <none>
2020-05-29 18:28:10 +0000

    Rewound to commit bed2130de1022e2168a0f77516f60ed39dc5e375 + 0 edits

--- komutlar/resimli-hgbb.js
+++ komutlar/resimli-hgbb.js
@@ -1,36 +1,36 @@
 const Discord = require('discord.js')
 const fs = require('fs');
 const ayarlar = require('../ayarlar.json');
-let kanal = JSON.parse(fs.readFileSync("./ayarlar/gç.json", "utf8"));
- 
+let kanal = JSON.parse(fs.readFileSync("./ayarlar/glog.json", "utf8"));
+var prefix = ayarlar.prefix;
+
 exports.run = async (client, message, args) => {
 if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`Bu Komutu Kullanabilmek İçin **Yönetici** İznine Sahip Olmalısın!`);
- 
+  
   let channel = message.mentions.channels.first()
     if (!channel) {
-        message.channel.send(`:x: | Kullanım: **${ayarlar.prefix}hgbb #kanal**`)
+        message.channel.send(':x: | Kullanım: `t+hgbb-kanal #kanal`')
         return
     }
     if(!kanal[message.guild.id]){
         kanal[message.guild.id] = {
-   resim: channel.id
+            resim: channel.id
         };
     }
-    fs.writeFile("./ayarlar/gç.json", JSON.stringify(kanal), (err) => {
+    fs.writeFile("./ayarlar/glog.json", JSON.stringify(kanal), (err) => {
         console.log(err)
     })
-    message.channel.send(`:white_check_mark: | ** Resimli Hoşgeldin - Güle Güle kanalı ${channel} Olarak Ayarlandı.** `)
+    message.channel.send(`:white_check_mark: | ** Resimli Hoşgeldin - Görüşürüz kanalı ${channel} Olarak Ayarlandı.** `)
 }
-   
+    
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: ["gç-ayarla"],
-    permLevel: 0
+    aliases: [],
+    permLevel: 2
 }
- 
 exports.help = {
-    name: 'hgbb',
+    name: 'hgbb-kanal',
     description: 'Giriş Çıkış Kanalını Ayarlar.',
-    usage: 'hgbb <#kanal>'
+    usage: 'resimli-giriş-çıkış <#kanal>'
 }
