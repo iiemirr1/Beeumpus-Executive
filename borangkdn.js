915fd17fb2ade0b77d0c4c455075a71faaba9e30
Glitch (doudfgr) <none>
2022-08-11 15:29:25 +0000

    🎱🙇 Checkpoint ./borangkdn.js:30335778/144

--- borangkdn.js
+++ borangkdn.js
@@ -46,6 +46,6 @@ readdir("./src/events", (err, files) => {
   });
 });
 
-client.login("MTAwNTQ4NDA2ODI5NDYzNTYwMA.GkO1nG.JUCXL8nYqcm-t8Gly_0SVFEnfaiaQZ8kOuyU0Q")
+client.login("MTAwNTQ4NDA2ODI5NDYzNTYwMA.G7tJ9d.6leRNhnBguiZIzx3FRZbhyVUQQtKA_Nu8O1VnU")
   .then(() => console.log(`Bot ${client.user.username} olarak giriş yaptı!`))
   .catch((err) => console.log(`Bot Giriş yapamadı sebep: ${err}`));
